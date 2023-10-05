31982549e4b7accfd9bf1487fb4e368f7fcefba4
DevLarry <olanrewajusamad070@gmail.com>
2023-10-05 23:26:36 +0100

    Sum everything

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 """Add the stuffs"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """computes and return the sum of two floats"""
     return sum(input_list)
+
+floats = [3.14, 1.11, 2.22]
+floats_sum = sum_list(floats)
+print(floats_sum == sum(floats))
+print(sum_list.__annotations__)
+print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_sum)))
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Add the stuffs"""
+from typing import Union, List
 
 
-def sum_mixed_list(input_list: list[float | int]) -> float:
+def sum_mixed_list(input_list: List(Union[float, int])) -> float:
     """computes and return the sum of two floats"""
     return sum(input_list)
