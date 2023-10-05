44ce6663e914f541003c435da19cb9ff96a9a322
DevLarry <olanrewajusamad070@gmail.com>
2023-10-05 23:46:30 +0100

    Sum everything

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Add the stuffs"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """computes and return the sum of two floats"""
+    return k, float(v)
