9410f594eafec47a6481bc63c2d71ef468fb244f
DevLarry <olanrewajusamad070@gmail.com>
2023-10-05 23:28:55 +0100

    Sum everything

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,9 +6,3 @@ from typing import List
 def sum_list(input_list: List[float]) -> float:
     """computes and return the sum of two floats"""
     return sum(input_list)
-
-floats = [3.14, 1.11, 2.22]
-floats_sum = sum_list(floats)
-print(floats_sum == sum(floats))
-print(sum_list.__annotations__)
-print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_sum)))
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import Union, List
 
 
-def sum_mixed_list(input_list: List(Union[float, int])) -> float:
+def sum_mixed_list(input_list: List[Union[float, int]]) -> float:
     """computes and return the sum of two floats"""
     return sum(input_list)
