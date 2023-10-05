744ebd04c653a259651d31189d441d8e86947cf9
DevLarry <olanrewajusamad070@gmail.com>
2023-10-05 23:38:15 +0100

    Sum everything

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,4 +5,4 @@ from typing import Union, List
 
 def sum_mixed_list(input_list: List[Union[float, int]]) -> float:
     """computes and return the sum of two floats"""
-    return sum(input_list)
+    return float(sum(input_list))
