0c8d6fc79bd3dda95cfb980eb43d1a4c581f5509
DevLarry <olanrewajusamad070@gmail.com>
2023-10-15 22:10:10 +0100

    Async advanced

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,10 +3,11 @@
 
 from random import uniform
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Function that does it all
     """
     result = []
