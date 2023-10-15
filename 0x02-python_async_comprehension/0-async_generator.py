b570a5b937036be28d7569e16d7852daeb5de000
DevLarry <olanrewajusamad070@gmail.com>
2023-10-15 21:50:58 +0100

    Async advanced

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,12 +9,3 @@ async def async_generator():
     for i in range(10):
         await asyncio.sleep(1)
         yield uniform(0, 10)
-
-if __name__ == "__main__":
-    async def print_yielded_values():
-        result = []
-        async for i in async_generator():
-            result.append(i)
-        print(result)
-
-    asyncio.run(print_yielded_values())
