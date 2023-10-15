0ab73a2a3b29bb7700b6ce8dfdb6c246a2a5ed6f
DevLarry <olanrewajusamad070@gmail.com>
2023-10-15 22:09:20 +0100

    Async advanced

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """Async generator"""
 
-from random import uniform
+from random import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Function"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield uniform(0, 10)
+        yield random() * 10
 
 if __name__ == "__main__":
     async def print_yielded_values():
